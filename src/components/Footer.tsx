5ae5176a023a1fe9cb61d18348121ba8743dfb82
avoytovich <levanwork@gmail.com>
2025-03-17 17:21:35 +0100

    add link book

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="text-center p-4 bg-gray-400 py-4">
       <p className="text-sm text-gray-700">
-        &copy; 2025 created by{' '}
+        Copyright &copy; 2025 Created by{' '}
         <Link
           href="https://portfolio-five-delta-66.vercel.app"
           className="text-blue-900 underline"
